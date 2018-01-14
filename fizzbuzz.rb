3b992b56ef88beb2d642764ed84c69089a5c2bbd
chandu11288@gmail.com <chandu11288@gmail.com>
2018-01-14 22:23:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(num)
  if num % 3 == 0 && num % 5 !=0
    return 'Fizz'
- elsif num % 5 == 0
+ elsif num % 5 == 0 && num % 3 !=0
    return 'Buzz'
  elsif num % 3 ==0 && num % 5 ==0
    return 'FizzBuzz'
